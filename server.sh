b55f7a43412af6a8cc4c8548959d83a82af97f76
Rachad ABI CHAHINE <rachad.abi-chahine@hotmail.fr>
2016-09-28 12:07:04 +0200

    updates php configuration

--- server.sh
+++ server.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get -y dist-upgrade
+
 sudo apt-get -y install apache2
 sudo apt-get -y install php5 php5-mysql libapache2-mod-php5 php5-curl php5-common php5-json php5-mysql php5-mcrypt php5-twig php5-sqlite php5-apcu php5-intl 
 sudo apt-get -y install phpunit
