1589374be4d6b417c4e7785b38a00d74afa84890
Rachad Abi Chahine <rachad.abi-chahine@hotmail.fr>
2016-12-28 22:25:46 +0100

    server.h: changes php version

--- server.sh
+++ server.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 sudo apt-get update
-
 sudo apt-get -y install apache2
-sudo apt-get -y install php5 php5-mysql libapache2-mod-php5 php5-curl php5-common php5-json php5-mysql php5-mcrypt php5-twig php5-sqlite php5-apcu php5-intl 
+sudo apt-get -y install php7.0 php7.0-mysql libapache2-mod-php7.0 php7.0-curl php7.0-common php7.0-json php7.0-mysql php7.0-mcrypt php7.0-twig php7.0-sqlite php7.0-apcu php7.0-intl 
 sudo apt-get -y install phpunit
-sudo apt-get -y install mysql-server php5-mysql
+sudo apt-get -y install mysql-server php7.0-mysql
 sudo apt-get -y install phpmyadmin
 sudo apt-get -y install sqlite3
-sudo apt-get -y install php5-sqlite
-sudo apt-get -y install php5-curl
+sudo apt-get -y install php7.0-sqlite
+sudo apt-get -y install php7.0-curl
 echo 'Include /etc/phpmyadmin/apache.conf'| sudo tee -a /etc/apache2/apache2.conf
 sudo systemctl restart apache2
-sudo apt-get -y install git
-#on install curl pour pouvoir installer composer.
 sudo apt-get -y install curl
 sudo curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
-sudo systemctl restart apache2
-#alecsia utilise bsdtar pour les archives
-sudo apt-get install bsdtar
+
 
