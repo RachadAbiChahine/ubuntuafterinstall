9a61b9d03188f80301f4e86125fc106b19e9c07f
Rachad Abi Chahine <rachad.abi-chahine@hotmail.fr>
2017-12-03 17:12:37 +0100

    server.sh : update php to 7.1

--- server.sh
+++ server.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get -y install apache2
-sudo apt-get -y install php7.0 php7.0-mysql libapache2-mod-php7.0 php7.0-curl php7.0-common php7.0-json php7.0-mysql php7.0-mcrypt php7.0-twig php7.0-sqlite php7.0-apcu php7.0-intl 
+sudo apt-get -y install php7.1 php7.1-mysql libapache2-mod-php7.1 php7.1-curl php7.1-common php7.1-json php7.1-mysql php7.1-mcrypt php7.1-twig php7.1-sqlite php7.1-apcu php7.1-intl 
 sudo apt-get -y install phpunit
-sudo apt-get -y install mysql-server php7.0-mysql
+sudo apt-get -y install mysql-server php7.1-mysql
 sudo apt-get -y install phpmyadmin
 sudo apt-get -y install sqlite3
-sudo apt-get -y install php7.0-sqlite
-sudo apt-get -y install php7.0-curl
+sudo apt-get -y install php7.1-sqlite
+sudo apt-get -y install php7.1-curl
 echo 'Include /etc/phpmyadmin/apache.conf'| sudo tee -a /etc/apache2/apache2.conf
 sudo systemctl restart apache2
 sudo apt-get -y install curl
